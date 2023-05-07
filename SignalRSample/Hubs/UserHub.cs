2961bec4d1d0bba685cf7aea7aea0860f28b6a0a
Jake Lin <jake.lin@dash.com.au>
2023-05-07 10:49:02 +1000

    Second Step - Add Method to Hub

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -2,8 +2,18 @@
 
 namespace SignalRSample.Hubs
 {
+    /// <summary>
+    /// Count number of views in the webpage
+    /// </summary>
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
 
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            // Send update to all clients that total views have been updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
     }
 }
