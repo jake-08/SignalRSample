354e41b3f695d0fa8bc8d06ff7ececea875b3cf8
Jake Lin <jake.lin@dash.com.au>
2023-05-17 22:49:52 +1000

    Add UsersCount

--- SignalRSample/wwwroot/js/usersCount.js
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -1,5 +1,8 @@
-// Create Connection
-var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+// Create Connection to SignalR Hub using the route provided in Program.cs 
+var connectionUserCount = new signalR.HubConnectionBuilder()
+    //.configureLogging(signalR.LogLevel.Information)
+    .withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build();
 
 // Connect to methods that hub invokes aka receive notifications from hub
 connectionUserCount.on("updateTotalViews", (value) => {
@@ -7,18 +10,24 @@ connectionUserCount.on("updateTotalViews", (value) => {
     newCountSpan.innerText = value.toString();
 });
 
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
 // Invoke hub methods aka send notification to hub
 function newWindowLoadedOnClient() {
-    connectionUserCount.send("NewWindowLoaded");
+    connectionUserCount.invoke("NewWindowLoaded", "Jake").then((value) => console.log(value));
 }
 
 // Start Connection
 function fulfilled() {
     console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient(); 
 }
 
 function rejected() {
 
 }
 
-connectionUserCount.start().then(fulfilled, rejected)
+connectionUserCount.start().then(fulfilled, rejected);
